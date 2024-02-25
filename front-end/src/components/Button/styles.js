95e8cb520e9350557d5067af0eacef7c63c0769b
Joao Pedro <joaopedrolamim@yahoo.com.br>
2024-02-25 16:17:12 -0300

    Created component button

---
+++ front-end/src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const Container = styled.button`
+  width: 100%;
+  height: 56px;
+
+  background-color: ${({ theme }) => theme.COLORS.PINK};
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+
+  border: none;
+  border-radius: 10px;
+  padding: 0 16px;
+  font-weight: 500;
+  margin-top: 16px;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+`
